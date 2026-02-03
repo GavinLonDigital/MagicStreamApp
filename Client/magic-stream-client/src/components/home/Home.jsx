6f65f637d93916f8986d1b6aa31550a8d921092f
Gavin Lon <42768005+GavinLonDigital@users.noreply.github.com>
2026-02-03 11:19:57 +0200

    Minor heading change

--- Client/magic-stream-client/src/components/home/Home.jsx
+++ Client/magic-stream-client/src/components/home/Home.jsx
@@ -31,6 +31,7 @@ const Home =({updateMovieReview}) => {
 
     return (
         <>
+            <h2>Magic STream Heading</h2>
             {loading ? (
                 <Spinner/>
             ):  (
